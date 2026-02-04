9caf3310e7e46d3e90455a3cf2b30ff35be69734
Parth Gawade <gawadeparth777@gmail.com>
2026-02-04 16:03:21 +0000

    Add multiplication table generation program

--- Assignment_3.c
+++ Assignment_3.c
@@ -4,4 +4,8 @@ int main() {
     int num, i;
     printf("enter a number:");
     scanf("%d",&num);
-    
+    for (i=1; i<=10; i++) {
+        printf("%d x %d = %d\n",num,i,num*i);
+    }
+    return 0;
+}
