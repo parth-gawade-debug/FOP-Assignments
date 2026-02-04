61275e8e0c416419e81d409c8d3cb8224300faba
Parth Gawade <gawadeparth777@gmail.com>
2026-02-04 15:57:28 +0000

    Fix leap year logic and improve output formatting; add initial arithmetic operations menu

--- Assignment_1.c
+++ Assignment_1.c
@@ -4,11 +4,11 @@ int main() {
     int year;
     printf("enter a year:");
     scanf("%d",&year);
-    if (year%4==0 && year%100!=0) {
-        printf("It is a leap year %d\n",year);
+    if (year%400==0 || (year%4==0 && year%100!=0)) {
+        printf("%d\n is a leap year",year);
     }
     else {
-        printf("It is not a leap year");
+        printf("%d\n is not a leap year,",year);
     }
     return 0;
 }
---
+++ Assignment_2.c
@@ -0,0 +1,17 @@
+// write a menu driven program to implement the basic arithmetic operations(addition, subtraction, multiplication, division) using switch case.
+#include <stdio.h>
+int main() {
+    float num1;
+    float num2;
+    int ch;
+     printf("-------menu-------\n");
+    printf("press 1 for addition\n");
+    printf("press 2 for subtraction\n");
+    printf("press 3 for multiplication\n");
+    printf("press 4 for division\n");
+    printf("enter your preference:");
+
+    scanf("%f",&num1);
+    scanf("%f",&num2);
+    scanf("%d",&ch);
+    
