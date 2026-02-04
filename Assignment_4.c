d4f6ad9ad4e6aeeaf4a033efb208df4ef88b02fc
Parth Gawade <gawadeparth777@gmail.com>
2026-02-04 16:06:32 +0000

    Add program to calculate employee salary based on basic salary and grade

---
+++ Assignment_4.c
@@ -0,0 +1,7 @@
+// program to calculate the salary of an employee based on the basic salary and the grade.
+#include <stdio.h>
+int main() {
+    float basic_salary, HRA, TA, PT, gross_salary, net_salary;
+    printf("enter the basic salary:");
+    scanf("%f",&basic_salary);
+    
