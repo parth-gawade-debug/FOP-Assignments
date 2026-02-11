358bee85c27e9b9d2d3ea2c98a0a507105bdf2fa
Parth Gawade <gawadeparth777@gmail.com>
2026-02-11 13:58:29 +0000

    Refactor factorial function to improve readability and maintainability

--- Assignment_10.c
+++ Assignment_10.c
@@ -3,6 +3,13 @@
 
 // Function declaration
 int factorial(int num);
+int factorial(int num) {
+    if(num == 0 || num == 1) {
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
+}
 
 int main() {
     float num1, num2, result;
@@ -50,13 +57,4 @@ int main() {
     }
     
     return 0;
-}
-
-
-int factorial(int num) {
-    if(num == 0 || num == 1) {
-        return 1;
-    } else {
-        return num * factorial(num - 1);
-    }
 }
